83c3046ce5464f927efe0e8170e52fdd27111982
ronspierings <ron@r-spierings.nl>
2018-03-27 09:35:05 +0200

    Added PHP eindopdracht P3

---
+++ php/Periode 03/productie/Eindopdracht.php
@@ -0,0 +1,14 @@
+#### [kleurcode]rgba(63,81,181,1)
+
+# Eindopdracht periode 1
+---
+### Download
+
+<a href="https://elo.kw1c.nl/CMS/Studie/811%20ICT-Academie/811%20VakkenInhoud/%5BB.22%20PHP%5D%20PHP/25187%20%C2%A0%20Applicatie-%20en%20mediaontwikkelaar/Periode%2003/Productie/02.%20Opdrachten/Eindopdracht%201.pdf" target="_blank">Download eindopdracht</a>
+
+### Onderwerpen
+- HTML Formulieren
+- $_GET parameters
+- Associatieve Arrays
+- foreach()
+- print_r()
